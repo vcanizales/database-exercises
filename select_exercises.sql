ab61485c0bab61d2a686521f9e88b72843d5c7b5
Valeria Canizales <vcanizales93@gmail.com>
2022-11-07 11:47:08 -0600

    Located genre of album and albums released in the 90s

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,3 +2,5 @@ USE codeup_test_db;
 
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Thriller';
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 2000;
