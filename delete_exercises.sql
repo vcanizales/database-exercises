4bb7106aed8983e780d551c7fc600dd91b78dcd6
Valeria Canizales <vcanizales93@gmail.com>
2022-11-07 13:56:57 -0600

    select statements for delete exercises a

---
+++ delete_exercises.sql
@@ -0,0 +1,3 @@
+USE codeup_test_db;
+
+SELECT * FROM albums WHERE release_date > 1991;
--- update_exercises.sql
+++ update_exercises.sql
@@ -2,7 +2,12 @@ USE codeup_test_db;
 
 SELECT * FROM albums;
 UPDATE albums SET sales = sales * 10;
+SELECT * FROM albums;
+
 SELECT * FROM albums WHERE release_date <= 1980;
 UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
+SELECT * FROM albums;
+
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET artist = 'Peter Jackson' WHERE id in (8, 17, 35, 43);
+SELECT * FROM albums;
