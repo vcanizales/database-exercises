b8b0c12e382785d21d0e61641c2f31c4692883ae
Valeria Canizales <vcanizales93@gmail.com>
2022-11-07 11:50:11 -0600

    Display rock albums only

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,4 @@ SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely hearts Club Ba
 SELECT genre FROM albums WHERE name = 'Thriller';
 SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 2000;
 SELECT name FROM albums WHERE sales < 20;
+SELECT * FROM albums WHERE genre = 'rock';
