f1d87869d4f7e1c027fb1c1614c2555ece41ff26
Valeria Canizales <vcanizales93@gmail.com>
2022-11-07 11:48:24 -0600

    Less than 20million in sales

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,4 @@ SELECT * FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely hearts Club Band';
 SELECT genre FROM albums WHERE name = 'Thriller';
 SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+SELECT name FROM albums WHERE sales < 20;
