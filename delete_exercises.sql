311c045b9d6b79dd9fb05b4e338a1b53d5603300
Valeria Canizales <vcanizales93@gmail.com>
2022-11-07 14:09:48 -0600

    DELETE commands finished exercise for delete_exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,3 +1,5 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE artist = 'Pink Floyd';
