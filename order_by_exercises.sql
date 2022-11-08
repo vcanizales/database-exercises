8528d4034d5d6a15a5a4d9a1ffc2df31b24859fe
Valeria Canizales <vcanizales93@gmail.com>
2022-11-08 12:17:19 -0600

    Order by exercises 5

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,6 +6,8 @@ SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR fi
 
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY last_name, first_name;
 
+SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no;
+
 SELECT * FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E';
 
 # SELECT * FROM employees WHERE last_name LIKE '%q%';
