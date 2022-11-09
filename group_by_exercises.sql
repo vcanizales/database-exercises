18fa56718d4a465117c4dcde7bd0def7a9b62a20
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 11:23:50 -0600

    updated query for names and added count for gender

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,3 +10,5 @@ SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'E%' a
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 SELECT COUNT(emp_no), last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT COUNT(*), gender FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender;
