86a7c8ddc247eef2020fed8f262e570483b2ca8b
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 09:14:32 -0600

    employees hired in the 90s and born on christmas DESC

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -17,4 +17,4 @@ SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
 
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
 
-SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date;
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC;
