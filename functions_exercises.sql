48c4d369bcea6c6d953d37b5805ec60f77760b47
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 10:32:59 -0600

    finding unique last names that start and end with E using GroupBy

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -17,6 +17,6 @@ SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
 
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
 
-SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC;
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
 
-SELECT DATEDIFF(NOW(), hire_date ) FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC;
+SELECT *, DATEDIFF(NOW(), hire_date ) FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY DATEDIFF(NOW(), hire_date) DESC;
