7c612088ae8f7680b5b73fb82e6cfbc146921db0
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 09:53:19 -0600

    finding unique first and last names that start and end with E

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -3,3 +3,5 @@ USE employees;
 SELECT DISTINCT title FROM titles;
 
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E' GROUP BY last_name;
+
+SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E' GROUP BY first_name, last_name;
