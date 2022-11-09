43c34344b08dc699a81c5b169b57aab38809668a
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 12:08:15 -0600

    10 employees in full name format added DOB

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -2,4 +2,6 @@ USE employees;
 
 SELECT CONCAT(last_name,' ',first_name) AS full_name FROM employees LIMIT 10;
 
+SELECT CONCAT(last_name,' ',first_name) AS `FULL NAME`, birth_date AS `DOB` FROM employees LIMIT 10;
+
 
