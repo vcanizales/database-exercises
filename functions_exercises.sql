51868de03ebbc56a1432700b732111421454d0da
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 09:16:16 -0600

    number of days employees have worked at company for datediff()

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -18,3 +18,5 @@ SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
 
 SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC;
+
+SELECT DATEDIFF(NOW(), hire_date ) FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC;
