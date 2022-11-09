9398d6a550fcdbdd61ec54508fdfd68d6a2cb4f7
Valeria Canizales <vcanizales93@gmail.com>
2022-11-09 11:10:52 -0600

    unique last names with q and not a qu

---
+++ aggregate_functions_lecture.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+SELECT COUNT(first_name), gender FROM employees GROUP BY gender;
+#This will not work
+SELECT first_name FROM employees GROUP BY gender;
+
+SELECT AVG(DATEDIFF(hire_date, birth_date))/365 FROM employees;
+
+#Youngest employee hired
+SELECT MIN(DATEDIFF(hire_date, birth_date))/365 FROM employees;
+
+#Oldest employee hired
+SELECT MAX(DATEDIFF(hire_date, birth_date))/365 FROM employees;
+
+
+#Identitical outcomes for the next 2 commands
+SELECT first_name FROM employees GROUP BY first_name;
+SELECT DISTINCT first_name FROM employees GROUP BY first_name;
+
+SELECT COUNT(first_name), first_name FROM employees GROUP BY first_name;
+
+# SELECT COUNT(first_name), first_name FROM employees GROUP BY first_name ORDER BY COUNT(first_name) DESC;
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -5,3 +5,5 @@ SELECT DISTINCT title FROM titles;
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E' GROUP BY last_name;
 
 SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E' GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q' LIKE 'q%' LIKE '%q%' NOT LIKE '%qu' NOT LIKE 'qu%' NOT LIKE '%qu%'GROUP BY last_name;
