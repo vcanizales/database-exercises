c4116aece0ab72709e8c5a4f0daaab3db84369b9
Valeria Canizales <vcanizales93@gmail.com>
2022-11-14 13:38:32 -0600

    subquries exercises complete with bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -21,3 +21,26 @@ SET role_id = (SELECT id FROM roles WHERE name = 'admin')
 WHERE email = 'fer@codeup.com';
 
 SELECT * FROM employees WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = '101010');
+
+SELECT * FROM titles WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN (
+    SELECT emp_no FROM dept_manager WHERE emp_no IN (
+        SELECT emp_no FROM employees WHERE gender = 'f'
+        )
+    AND to_date > CURDATE()
+    );
+
+##############BONUS#################
+
+SELECT dept_name FROM departments WHERE dept_no IN (
+    SELECT dept_no FROM dept_manager WHERE emp_no IN (
+        SELECT emp_no FROM employees WHERE gender = 'F'
+        )
+    AND to_date > CURDATE()
+    );
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN (
+    SELECT emp_no FROM salaries WHERE salary IN (
+        SELECT max(salary) FROM salaries
+        ));
