b40e5cfa25f048cb6dfbc6d65a7de25c0192bc06
Valeria Canizales <vcanizales93@gmail.com>
2022-11-14 10:08:49 -0600

    Join exercises and bonus completed

--- join_exercises.sql
+++ join_exercises.sql
@@ -28,12 +28,11 @@ SELECT dept_name AS `Department Name`, CONCAT(first_name,' ',last_name) AS `Depa
     WHERE dm.to_date > CURDATE() ORDER BY dept_name;
 
 # Find the name of all departments currently managed by women
-SELECT e.first_name, e.last_name FROM employees e
-    JOIN dept_manager dm on e.emp_no = dm.emp_no
-    JOIN departments d on dm.dept_no = d.dept_no
-    WHERE dm.to_date> CURDATE()
-    AND gender = 'f';
-
+SELECT dept_name AS `Department Name`, CONCAT(first_name,' ',last_name) AS `Department Manager` FROM employees
+    JOIN dept_manager dm on employees.emp_no = dm.emp_no
+    JOIN departments d ON dm.dept_no = d.dept_no
+    WHERE dm.to_date LIKE '9%'
+     AND gender = 'f' ORDER BY dept_name;
 
 # Find the current titles of employees currently working in the Customer Service department.
 SELECT t.title AS `Title`, COUNT(*) AS `Total` FROM titles as t
@@ -45,7 +44,7 @@ SELECT t.title AS `Title`, COUNT(*) AS `Total` FROM titles as t
 
 
 # Find the current salary of all current managers.
-SELECT dept_name AS `Department Name`, CONCAT(first_name, last_name) AS `Department Manager`, salary AS `Salary` FROM employees
+SELECT dept_name AS `Department Name`, CONCAT(first_name,' ', last_name) AS `Department Manager`, salary AS `Salary` FROM employees
     JOIN dept_manager dm ON employees.emp_no = dm.emp_no
     JOIN departments d ON dm.dept_no = d.dept_no
     JOIN salaries s on employees.emp_no = s.emp_no
@@ -56,8 +55,17 @@ SELECT dept_name AS `Department Name`, CONCAT(first_name, last_name) AS `Departm
 
 # Bonus Find the names of all current employees, their department name, and their current manager's name .
 
-SELECT CONCAT(first_name, ' ',last_name) AS `Employee`, d.dept_name AS `Department`, CONCAT(first_name,' ',last_name) AS `Department Manager` FROM employees
-    JOIN dept_emp emp ON emp.emp_no = emp.emp_no
-    JOIN departments d ON emp.dept_no = d.dept_no
-    JOIN dept_manager dm ON employees.emp_no = dm.emp_no
-    WHERE emp.to_date = '9999-01-01' AND dm.to_date = '9999-01-01';
+# SELECT CONCAT(first_name, ' ',last_name) AS `Employee`, d.dept_name AS `Department`, CONCAT(first_name,' ',last_name) AS `Department Manager` FROM employees
+#     JOIN dept_emp emp ON emp.emp_no = emp.emp_no
+#     JOIN departments d ON emp.dept_no = d.dept_no
+#     JOIN dept_manager dm ON employees.emp_no = dm.emp_no
+#     WHERE emp.to_date = '9999-01-01' AND dm.to_date = '9999-01-01';
+
+SELECT CONCAT(e.first_name, ' ',e.last_name) AS Employee, d.dept_name AS Department, CONCAT(m.first_name, ' ', m.last_name) AS Manager
+FROM employees e
+         LEFT JOIN dept_emp AS de ON e.emp_no = de.emp_no
+         LEFT JOIN departments d on de.dept_no = d.dept_no
+         LEFT JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees AS m on dm.emp_no = m.emp_no
+WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+ORDER BY dept_name asc;
